41623c8675968e86dc3c172a7b0ea24fddbee36c
Pacahar <s.kirill29@gmail.com>
2025-05-07 19:26:00 +0300

    Add possibility for author to check all responses to specific survey with /surveys/<survey_id>/responses

--- askme-backend/surveys/views.py
+++ askme-backend/surveys/views.py
@@ -1,6 +1,9 @@
 from rest_framework import viewsets, permissions
-from rest_framework.exceptions import PermissionDenied
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework.exceptions import PermissionDenied, NotFound
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.response import Response as DRFResponse
+
 
 from .permissions import IsSurveyCreator
 from .serializers import SurveySerializer, QuestionSerializer, ResponseSerializer, OptionSerializer
@@ -10,29 +13,38 @@ from .models import Survey, Question, Response, Option
 class SurveyViewSet(viewsets.ModelViewSet):
     serializer_class = SurveySerializer
 
-
     def get_permissions(self):
         if self.action == 'list':
             return [permissions.IsAuthenticated()]
         return []
-
 
     def get_queryset(self):
         if self.action == 'list':
             return Survey.objects.filter(creator=self.request.user)
         return Survey.objects.all()
 
-
     def perform_create(self, serializer):
         if not self.request.user.is_authenticated:
             raise PermissionDenied("Only authorized users can create surveys.")
         serializer.save(creator=self.request.user)
+
+    @action(detail=True, methods=['get'], url_path='responses', permission_classes=[permissions.IsAuthenticated])
+    def responses(self, request, pk=None):
+        survey = self.get_object()
+
+        if survey.creator != request.user:
+            raise PermissionDenied("Access denied. You are not the author of this survey.")
+
+        responses = Response.objects.filter(survey=survey)
+        serializer = ResponseSerializer(responses, many=True)
+        return DRFResponse(serializer.data)
 
 
 class QuestionViewSet(viewsets.ModelViewSet):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
     permission_classes = [IsAuthenticated, IsSurveyCreator]
+
 
     def perform_create(self, serializer):
         survey_id = self.request.data.get("survey")
@@ -50,10 +62,18 @@ class ResponseViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.AllowAny]
 
 
+    def get_queryset(self):
+        user = self.request.user
+        if not user.is_authenticated:
+            raise PermissionDenied("Only authorized users can get responses info.")
+        return super().get_queryset().filter(survey__creator=user)
+
+
 class OptionViewSet(viewsets.ModelViewSet):
     queryset = Option.objects.all()
     serializer_class = OptionSerializer
     permission_classes = [IsAuthenticated, IsSurveyCreator]
+
 
     def perform_create(self, serializer):
         question_id = self.request.data.get("question")
